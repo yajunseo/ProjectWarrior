ebddd6e715a6c179563068fefb9e7ed4b2d5e510
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2025-06-17 00:22:06 +0900

    게임모드, 컨트롤러 생성

---
+++ ProjectWarrior/Source/ProjectWarrior/Public/Characters/WarriorHeroCharacter.h
@@ -0,0 +1,17 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "WarriorHeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTWARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
