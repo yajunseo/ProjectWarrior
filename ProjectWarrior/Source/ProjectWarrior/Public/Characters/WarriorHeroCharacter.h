95f9ff9d537ffbc8e0393e9692b069b7a1fde55b
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2025-06-22 22:59:34 +0900

    InputAction, GamePlayTag 연결

--- ProjectWarrior/Source/ProjectWarrior/Public/Characters/WarriorHeroCharacter.h
+++ ProjectWarrior/Source/ProjectWarrior/Public/Characters/WarriorHeroCharacter.h
@@ -6,6 +6,9 @@
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
+
+class USpringArmComponent;
+class UCameraComponent;
 /**
  * 
  */
@@ -13,5 +16,21 @@ UCLASS()
 class PROJECTWARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AWarriorHeroCharacter();
 	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+	
+#pragma endregion
 };
