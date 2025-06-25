9839f7096fa0a76183f6338c1a9073e1f8eb3695
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2025-06-26 00:15:23 +0900

    relax 애니메이션 적용

--- ProjectWarrior/Source/ProjectWarrior/Private/AnimInstance/Hero/WarriorHeroAnimInstance.cpp
+++ ProjectWarrior/Source/ProjectWarrior/Private/AnimInstance/Hero/WarriorHeroAnimInstance.cpp
@@ -2,4 +2,32 @@
 
 
 #include "AnimInstance/Hero/WarriorHeroAnimInstance.h"
+#include "Characters/WarriorHeroCharacter.h"
 
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+	
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		bShoudlEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElapsedTime += DeltaSeconds;
+
+		bShoudlEnterRelaxState = (IdleElapsedTime >= EnterRelaxStateThreshold);
+	}
+}
--- ProjectWarrior/Source/ProjectWarrior/Public/AnimInstance/Hero/WarriorHeroAnimInstance.h
+++ ProjectWarrior/Source/ProjectWarrior/Public/AnimInstance/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstance/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorBaseCharacter;
 /**
  * 
  */
@@ -13,5 +14,20 @@ UCLASS()
 class PROJECTWARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+	AWarriorBaseCharacter* OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShoudlEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
 	
+	float IdleElapsedTime;
 };
