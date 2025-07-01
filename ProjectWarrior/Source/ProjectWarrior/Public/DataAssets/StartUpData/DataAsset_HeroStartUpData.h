0ad8cf01b33f0cf716cda85669361412d15ed54c
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2025-07-02 00:08:25 +0900

    StartUpData 생성

---
+++ ProjectWarrior/Source/ProjectWarrior/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
@@ -0,0 +1,17 @@
+// Vince Petrelli All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "DataAsset_HeroStartUpData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTWARRIOR_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataBase
+{
+	GENERATED_BODY()
+	
+};
