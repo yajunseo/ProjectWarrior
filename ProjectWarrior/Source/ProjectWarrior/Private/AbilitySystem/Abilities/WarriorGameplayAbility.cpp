ace92a63f2f79fc46ce7ce4a4060c09f47c36b37
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2025-07-08 00:19:12 +0900

    HeroGameplayAbility 생성

--- ProjectWarrior/Source/ProjectWarrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
+++ ProjectWarrior/Source/ProjectWarrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
@@ -38,3 +38,8 @@ UPawnCombatComponent* UWarriorGameplayAbility::GetPawnCombatComponentFromActorIn
 {
 	return GetAvatarActorFromActorInfo()->FindComponentByClass<UPawnCombatComponent>();
 }
+
+UWarriorAbilitySystemComponent* UWarriorGameplayAbility::GetWarriorAbilitySystemComponentFromActorInfo() const
+{
+	return Cast<UWarriorAbilitySystemComponent>(CurrentActorInfo->AbilitySystemComponent);
+}
--- ProjectWarrior/Source/ProjectWarrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
+++ ProjectWarrior/Source/ProjectWarrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -7,6 +7,7 @@
 #include "WarriorGameplayAbility.generated.h"
 
 class UPawnCombatComponent;
+class UWarriorAbilitySystemComponent;
 
 UENUM(BlueprintType)
 enum class EWarriorAbilityActivationPolicy : uint8
@@ -33,4 +34,7 @@ protected:
 
 	UFUNCTION(BlueprintPure, Category = "Warrior|Ability")
 	UPawnCombatComponent* GetPawnCombatComponentFromActorInfo() const;
+
+	UFUNCTION(BlueprintPure, Category = "Warrior|Ability")
+	UWarriorAbilitySystemComponent* GetWarriorAbilitySystemComponentFromActorInfo() const;
 };
